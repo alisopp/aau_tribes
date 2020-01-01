a9497cbc124eba58f5fd8ac826407f71c4ce5c0b
Alexander Isopp <alisopp@edu.aau.at>
2020-01-01 22:33:01 +0100

    initial commit

---
+++ android/app/src/main/kotlin/com/tribegames/aau_tribes/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.tribegames.aau_tribes
+
+import androidx.annotation.NonNull;
+import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.engine.FlutterEngine
+import io.flutter.plugins.GeneratedPluginRegistrant
+
+class MainActivity: FlutterActivity() {
+    override fun configureFlutterEngine(@NonNull flutterEngine: FlutterEngine) {
+        GeneratedPluginRegistrant.registerWith(flutterEngine);
+    }
+}
