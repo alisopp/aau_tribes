def47357df8d72eba332f3422eb4b158e63182b9
Alexander Isopp <alisopp@edu.aau.at>
2020-01-02 23:59:18 +0100

    added config

---
+++ lib/config.dart
@@ -0,0 +1,9 @@
+const awsUserPoolId = '';
+const awsClientId = '';
+
+const identityPoolId = 'eu-central-1:xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx';
+
+// Setup endpoints here:
+const region = '';
+const endpoint =
+    'https://xxxxxxxxxx.execute-api.ap-southeast-1.amazonaws.com/dev';
