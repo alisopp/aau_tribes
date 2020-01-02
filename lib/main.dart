3d1c37da8e4371ec356315591ddb0cd839bceac5
Alexander Isopp <alisopp@edu.aau.at>
2020-01-02 15:10:43 +0100

    created skeleton application

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,15 @@
+import 'package:aau_tribes/register_page.dart';
+import 'package:amazon_cognito_identity_dart/cognito.dart';
 import 'package:flutter/material.dart';
+import 'authentification.dart';
+import 'confirmation_page.dart';
+import 'login_page.dart';
+
+
 
 void main() => runApp(MyApp());
+
+
 
 class MyApp extends StatelessWidget {
   // This widget is the root of your application.
@@ -20,92 +29,111 @@ class MyApp extends StatelessWidget {
         // is not restarted.
         primarySwatch: Colors.blue,
       ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      home: HomePage(title: 'Flutter Demo Home Page'),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
+class HomePage extends StatefulWidget {
+HomePage({Key key, this.title}) : super(key: key);
 
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
+final String title;
 
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
+@override
+_HomePageState createState() => new _HomePageState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
-
+class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+    final Size screenSize = MediaQuery.of(context).size;
+    return new Scaffold(
+      appBar: new AppBar(
+        title: new Text(widget.title),
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
+      body: new Center(
+        child: new Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
+            new Container(
+              padding:
+              new EdgeInsets.only(left: 20.0, right: 20.0, bottom: 20.0),
+              width: screenSize.width,
+              child: new RaisedButton(
+                child: new Text(
+                  'Sign Up',
+                  style: new TextStyle(color: Colors.white),
+                ),
+                onPressed: () {
+                  Navigator.push(
+                    context,
+                    new MaterialPageRoute(
+                        builder: (context) => new SignUpScreen(userPool: userPool)),
+                  );
+                },
+                color: Colors.blue,
+              ),
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.display1,
+            new Container(
+              padding:
+              new EdgeInsets.only(left: 20.0, right: 20.0, bottom: 20.0),
+              width: screenSize.width,
+              child: new RaisedButton(
+                child: new Text(
+                  'Confirm Account',
+                  style: new TextStyle(color: Colors.white),
+                ),
+                onPressed: () {
+                  Navigator.push(
+                    context,
+                    new MaterialPageRoute(
+                        builder: (context) => new ConfirmationScreen(userPool: userPool)),
+                  );
+                },
+                color: Colors.blue,
+              ),
             ),
+            new Container(
+              padding:
+              new EdgeInsets.only(left: 20.0, right: 20.0, bottom: 20.0),
+              width: screenSize.width,
+              child: new RaisedButton(
+                child: new Text(
+                  'Login',
+                  style: new TextStyle(color: Colors.white),
+                ),
+                onPressed: () {
+                  Navigator.push(
+                    context,
+                    new MaterialPageRoute(
+                        builder: (context) => new LoginScreen(userPool: userPool)),
+                  );
+                },
+                color: Colors.blue,
+              ),
+            ),
+           /* new Container(
+              padding:
+              new EdgeInsets.only(left: 20.0, right: 20.0, bottom: 20.0),
+              width: screenSize.width,
+              child: new RaisedButton(
+                child: new Text(
+                  'Secure Counter',
+                  style: new TextStyle(color: Colors.white),
+                ),
+                onPressed: () {
+                  Navigator.push(
+                    context,
+                    new MaterialPageRoute(
+                        builder: (context) => new SecureCounterScreen()),
+                  );
+                },
+                color: Colors.blue,
+              ),
+            ), */
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
